9b8c4e4ef903f282aa0575ad96bc66c8508597e3
Christoph Franke <krito@krito.de>
2018-04-29 13:47:08 +0200

    track events only when api key exists

--- src/tracking/index.js
+++ src/tracking/index.js
@@ -3,46 +3,53 @@ import KeenTracking from 'keen-tracking'
 
 class Tracking {
   constructor() {
-    this.client = new KeenTracking({
-        projectId: '5ae50594c9e77c0001cc21e7',
-        writeKey: process.env.keen
-    });
+    this.key = process.env.keen;
+    if (this.key) {    
+      this.client = new KeenTracking({
+          projectId: '5ae50594c9e77c0001cc21e7',
+          writeKey: this.key
+      });
+    }
   }
 
-  pageView(title) {  
-    // Record an event
-    this.client.recordEvent('pageviews', {
-      title,
-      host: window.location.host,
-      ip_address: "${keen.ip}",
-      keen: {
-        addons: [{
-          name: "keen:ip_to_geo",
-          input: {
-            ip: "ip_address"
-          },
-          output: "keen.location"
-        }]
-      }  
-    });
+  pageView(title) {
+    if (this.key) {    
+      this.client.recordEvent('pageviews', {
+        title,
+        host: window.location.host,
+        ip_address: "${keen.ip}",
+        keen: {
+          addons: [{
+            name: "keen:ip_to_geo",
+            input: {
+              ip: "ip_address"
+            },
+            output: "keen.location"
+          }]
+        }  
+      });
+    }
   }
 
   game(players, gameInfo) {
-    this.client.recordEvent('games', {
-      players,
-      gameInfo,
-      host: window.location.host,
-      ip_address: "${keen.ip}",
-      keen: {
-        addons: [{
-          name: "keen:ip_to_geo",
-          input: {
-            ip: "ip_address"
-          },
-          output: "keen.location"
-        }]
-      }  
-    });  }
+    if (this.key) {    
+      this.client.recordEvent('games', {
+        players,
+        gameInfo,
+        host: window.location.host,
+        ip_address: "${keen.ip}",
+        keen: {
+          addons: [{
+            name: "keen:ip_to_geo",
+            input: {
+              ip: "ip_address"
+            },
+            output: "keen.location"
+          }]
+        }  
+      }); 
+    }
+  }
 }
 
 export default new Tracking();
