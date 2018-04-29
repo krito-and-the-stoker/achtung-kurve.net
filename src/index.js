d61b73f648a7bd5866f89d8712a4fa33e8bdbcae
Christoph Franke <krito@krito.de>
2018-04-29 04:00:23 +0200

    added error tracking with sentry

--- src/index.js
+++ src/index.js
@@ -2,9 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Game from './game.js';
 import Tracking from './tracking/index.js';
+import Raven from 'raven-js';
 
 import './index.css';
 
+Raven
+    .config('https://5af48842d6d643d3bde7a59f2fe81b42@sentry.io/1198155')
+    .install();
 
-ReactDOM.render(<Game />, document.getElementById('root'));
-Tracking.pageView('Home');
+Raven.context(function () {
+	ReactDOM.render(<Game />, document.getElementById('root'));
+	Tracking.pageView('Home');
+});
