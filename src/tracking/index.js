cd4a48f34e5fc8cf174b4a5e958ca53eb5b1af2d
Christoph Franke <krito@krito.de>
2018-05-01 00:26:37 +0200

    fixed not recording any data at all problem

--- src/tracking/index.js
+++ src/tracking/index.js
@@ -5,32 +5,28 @@ import uuid from 'uuid/v4'
 class Tracking {
   constructor() {
     this.uuid = uuid()
-    if (this.key) {
-      this.client = new KeenTracking({
-          projectId: '5ae50594c9e77c0001cc21e7',
-          writeKey: process.env.REACT_APP_KEEN_SECRET
-      });
-    }
+    this.client = new KeenTracking({
+        projectId: '5ae50594c9e77c0001cc21e7',
+        writeKey: process.env.REACT_APP_KEEN_SECRET
+    });
   }
 
   pageView(title) {
-    if (this.key) {    
-      this.client.recordEvent('pageviews', {
-        title,
-        host: window.location.host,
-        ip_address: "${keen.ip}",
-        sessionId: this.uuid,
-        keen: {
-          addons: [{
-            name: "keen:ip_to_geo",
-            input: {
-              ip: "ip_address"
-            },
-            output: "keen.location"
-          }]
-        }  
-      });
-    }
+    this.client.recordEvent('pageviews', {
+      title,
+      host: window.location.host,
+      ip_address: "${keen.ip}",
+      sessionId: this.uuid,
+      keen: {
+        addons: [{
+          name: "keen:ip_to_geo",
+          input: {
+            ip: "ip_address"
+          },
+          output: "keen.location"
+        }]
+      }  
+    });
   }
 
   game(players, gameInfo) {
@@ -38,15 +34,13 @@ class Tracking {
       .filter(player => player.active)
       .map(player => player.name)
 
-    if (this.key) {    
-      this.client.recordEvent('games', {
-        players: playerNames,
-        duration: gameInfo.duration,
-        gameNumber: gameInfo.gameNumber,
-        host: window.location.host,
-        sessionId: this.uuid,
-      }); 
-    }
+    this.client.recordEvent('games', {
+      numPlayers: playerNames.length,
+      duration: gameInfo.duration,
+      gameNumber: gameInfo.gameNumber,
+      host: window.location.host,
+      sessionId: this.uuid,
+    }); 
   }
 }
 
